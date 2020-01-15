329733181bacf5939197f083b5b7621e8f8ed914
mkaychuks <sachufusi@gmail.com>
2020-01-15 08:01:58 +0100

    official DRF documentation tutorial complete

---
+++ snippets/serializers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+from .models import Snippet
+
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name='snippet-highlight', format='html')
+
+    class Meta:
+        model = Snippet
+        fields = [
+            'url', 'id', 'title', 'highlight', 'code', 'linenos', 'language',
+            'style', 'owner'
+        ]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(
+        many=True, view_name='snippet-detail', read_only=True
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            'url', 'id', 'username', 'snippets'
+        ]
---
+++ snippets/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from .views import (
+    SnippetList, SnippetDetail,
+    UserDetail, UserList, api_root,
+    SnippetHighlight
+)
+
+
+urlpatterns = [
+    path('snippets/', SnippetList.as_view(), name='snippet-list'),
+    path('snippets/<int:pk>/', SnippetDetail.as_view(), name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/',
+         SnippetHighlight.as_view(), name='snippet-highlight'),
+    path('users/', UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserDetail.as_view(), name='user-detail'),
+    path('', api_root),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
